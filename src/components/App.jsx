8364347bfe0aa067ae352059cb3de4260ea8a2a0
Max Denishchuk <maks.denishchuk@gmail.com>
2022-10-06 21:33:59 +0300

    //

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,26 @@ import { Form } from './Form/Form';
 
 import { Users } from './Users/Users';
 
+const STORAGE_DATA = 'contacts';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  componentDidMount = () => {
+    const storageData = JSON.parse(localStorage.getItem(STORAGE_DATA));
+    if (storageData) {
+      this.setState({ contacts: storageData });
+    }
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem(STORAGE_DATA, JSON.stringify(contacts));
+    }
   };
 
   handleSubmit = ({ name, number }, actions) => {
